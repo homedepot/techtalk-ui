f3c6fd9911e0f594221feeeb4ae5256b0a7bfdc1
badwrong <daniel.heidenhain@gmail.com>
2021-06-03 13:53:35 -0400

    add express, update youtube links

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const port = 8080
+
+const liveData = JSON.parse(fs.readFileSync('docs/jsonContracts/live.json'))
+const replayData = JSON.parse(fs.readFileSync('docs/jsonContracts/replay.json'))
+
+app.get('/', (req, res) => {
+  res.send('Server is up!')
+})
+
+app.get('/live/v1/nextPresentation', (req, res) => {
+  res.json(liveData)
+})
+
+app.get('/replay/v1/pastPresentations', (req, res) => {
+  res.json(replayData)
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
